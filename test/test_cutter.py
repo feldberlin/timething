fix: Duration units fixed
Duration units are in model frames, samples, or in seconds. Introduced
some functions to convert these.

test/test_cutter.py
@@ -1,22 +1,42 @@
-import numpy as np
-
 from timething import align, cutter  # type: ignore
+
+import helper
 
 
 def test_pause_durations():
-    alignment = align.Alignment(
-        log_probs=np.zeros(1),
-        trellis=np.zeros(1),
-        path=np.zeros(1),
-        char_segments=[],
-        word_segments=[
-            align.Segment("hello", 2, 8, 1.0),
-            align.Segment("world", 11, 20, 1.0)
-        ],
-        n_frames=30,
-        n_samples=100,
-        ratio=0,
-    )
+    alignment = helper.alignment(30, [
+        align.Segment("hello", 2, 8, 1.0),
+        align.Segment("world", 11, 20, 1.0)
+    ])
 
     pauses = cutter.pause_durations(alignment)
     assert pauses == [3, 10]
+
+
+def test_no_cut():
+    alignment = helper.alignment(30, [
+        align.Segment("hello", 2, 8, 1.0),
+        align.Segment("world", 11, 20, 1.0)
+    ])
+
+    cuts = cutter.pause_cuts(alignment, cut_threshold=100)
+    assert len(cuts) == 1
+    assert cuts[0].label == "hello world"
+    assert cuts[0].start == 2
+    assert cuts[0].end == 20
+
+
+def test_one_cut():
+    alignment = helper.alignment(30, [
+        align.Segment("hello", 2, 8, 1.0),
+        align.Segment("world", 15, 30, 1.0)
+    ])
+
+    cuts = cutter.pause_cuts(alignment, cut_threshold=5)
+    assert len(cuts) == 2
+    assert cuts[0].label == "hello"
+    assert cuts[0].start == 2
+    assert cuts[0].end == 8
+    assert cuts[1].label == "world"
+    assert cuts[1].start == 15
+    assert cuts[1].end == 30